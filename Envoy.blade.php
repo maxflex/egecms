adbec7e3a16457c1bc575e945e133d392e96944d
Max Kolyadin <makcyxa-k@yandex.ru>
2016-09-26 19:03:59 +0300

    Laravel 5.3 upgrade

---
+++ Envoy.blade.php
@@ -0,0 +1,35 @@
+@servers(['web' => 'root@cms.ege-repetitor.ru'])
+
+@task('deploy')
+    cd /home/egecms
+    git pull github master
+    php artisan config:cache
+    php artisan route:cache
+@endtask
+
+@task('gulp')
+    cd /home/egecms
+    gulp --production
+@endtask
+
+@task('stash_and_pull')
+    cd /home/egecms
+    git stash
+    git pull github master
+@endtask
+
+@task('cache')
+    cd /home/egecms
+    php artisan config:cache
+    php artisan route:cache
+@endtask
+
+@task('migrate')
+    cd /home/egecms
+    php artisan migrate --force
+@endtask
+
+@task('stash')
+    cd /home/egecms
+    git stash
+@endtask
