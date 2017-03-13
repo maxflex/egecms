0d077f4c266a31ec063e23766f5c82a36a6ac745
Max Kolyadin <makcyxa-k@yandex.ru>
2017-03-13 14:12:35 +0300

    fix tags command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\Sync;
+use App\Console\Commands\FixTags;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Sync::class
+        Sync::class,
+        FixTags::class,
     ];
 
     /**
--- app/Http/Controllers/Api/SassController.php
+++ app/Http/Controllers/Api/SassController.php
@@ -47,7 +47,7 @@ class SassController extends Controller
     public function update(Request $request, $id)
     {
         Storage::disk('web_sass')->put($id, $request->text);
-        $output = shell_exec("./compile-web-sass.sh");
+        $output = shell_exec("cd " . env('WEB_PATH') . " 2>&1 && gulp sass 2>&1");
         Storage::disk('web_sass')->put('log.txt', $output);
     }
 }
