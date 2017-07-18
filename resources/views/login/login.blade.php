2488e3277d63ac8b1221754666b472dac7075442
Max Kolyadin <makcyxa-k@yandex.ru>
2017-07-18 10:48:24 +0300

    SMS verify

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,20 +1,17 @@
 @extends('login')
-
-
 @section('content')
-<center ng-app="Login" ng-controller="LoginCtrl">
-	<form class="form-signin" ng-submit="checkFields()" autocomplete="off">
+<center>
+	<div class="form-signin" autocomplete="off">
 <!-- 		<h2 class="form-signin-heading">Вход в систему</h2> -->
-		<input ng-disabled="sms_verification" type="text" id="inputLogin" class="form-control" placeholder="Логин" autofocus name="login" ng-model="login" autocomplete="off">
-		<input ng-disabled="sms_verification" type="password" id="inputPassword" class="form-control" placeholder="Пароль" name="password" ng-model="password" autocomplete="off">
-        <input type="text" id="sms-code" class="form-control" placeholder="код из смс" ng-model="$parent.code" autocomplete="off" ng-keyup="enter($event)" ng-if="sms_verification">
+		<input ng-disabled="sms_verification" type="text" id="inputLogin" class="form-control" placeholder="Логин" autofocus ng-model="login" autocomplete="off" ng-keyup="enter($event)">
+		<input ng-disabled="sms_verification" type="password" id="inputPassword" class="form-control" placeholder="Пароль" ng-model="password" autocomplete="off" ng-keyup="enter($event)">
+		<input type="text" id="sms-code" class="form-control" placeholder="код из смс" ng-model="$parent.code" autocomplete="off" ng-keyup="enter($event)" ng-if="sms_verification">
 
-		<button id="login-submit" data-style="zoom-in" ng-disabled="in_process" class="btn btn-lg btn-primary btn-block ladda-button" type="submit">
-            <span class="glyphicon glyphicon-lock"></span>
-            <span ng-show="!in_process">Войти</span>
-            <span ng-show="in_process">Вход</span>
+		<button id="login-submit" data-style="zoom-in" ng-disabled="in_process" class="btn btn-lg btn-primary btn-block ladda-button" type="submit" ng-click="checkFields()">
+			<span class="glyphicon glyphicon-lock"></span><span ng-show="!in_process">Войти</span>
+			<span ng-show="in_process">Вход</span>
 		</button>
-	</form>
+	</div>
 </center>
 <div class="g-recaptcha" data-sitekey="{{ config('captcha.site') }}" data-size="invisible" data-callback="captchaChecked"></div>
 <script src='https://www.google.com/recaptcha/api.js?hl=ru'></script>
